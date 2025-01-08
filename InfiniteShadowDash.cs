95970a745a3b5630452066000186bad048325506
Crab-Teodor <108952169+Crab-Teodor@users.noreply.github.com>
2025-01-08 20:40:21 +0300

    Add files via upload

---
+++ InfiniteShadowDash.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Modding;
+using UnityEngine;
+
+namespace InfiniteShadowDash
+{
+    public class InfiniteShadowDash : Mod
+    {
+        new public string GetName() => "Infinite Shadow Dash";
+        public override string GetVersion() => "1.0.0.0";
+        public override void Initialize()
+        {
+            ModHooks.HeroUpdateHook += OnHeroUpdate;
+        }
+        public void OnHeroUpdate()
+        {
+            HeroController hero = HeroController.instance;
+            if (hero != null)
+            {
+                if (InputHandler.Instance.inputActions.dash.IsPressed)
+                {
+                    hero.SHADOW_DASH_COOLDOWN = 0;
+                    hero.shadowRechargePrefab.SetActive(value: false);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# InfiniteShadowDash
- Removes the cooldown of shadow charge and its cooldown animation
+# Infinite Shadow Dash
+Removes the cooldown of shadow charge and its cooldown animation
